dfcd29eea28ccb55d452e9316379819892d2b3bf
Daniel Lahera <32450130+DaniL97@users.noreply.github.com>
2018-01-04 22:06:38 +0100

    Update AgenteDeInversiones.java

--- src/Banco/AgenteDeInversiones.java
+++ src/Banco/AgenteDeInversiones.java
@@ -5,19 +5,58 @@
  */
 package Banco;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author guill
+ * @author guill y dlahera
  */
 public class AgenteDeInversiones extends Persona{
-    
-    
-    //parte de las operaciones/////
 
-    public AgenteDeInversiones(String nombre, String DNI, boolean tipo) {
-        super(nombre, DNI, tipo);
-    }
-    
-    
-    //parte de las operaciones/////
+    	private ArrayList<Mensaje> operPendientes;
+    	private ArrayList<Mensaje> resultadoOper;
+
+    	public AgenteDeInversiones(String nombre, String DNI, boolean tipo) {
+        	super(nombre, DNI, tipo);
+		this.operPendientes = new ArrayList<Mensaje>();
+		this.resultadoOper = new ArrayList<Mensaje>();
+    	}
+
+	public void añadirMensaje(Mensaje nuevoMensaje) {
+        	operPendientes.add(nuevoMensaje);
+		
+   	}
+    	
+	public void solicitarCompra (string cliente, string empresa, double cantidadMaxima) {
+		mensaje mc = new Mensaje (cliente, empresa, cantidadMaxima)
+		if (bancoCli.nombre.!contains(mc.cliente)) {
+			throw new MensajeException (MensajeException.EX_CLIENTE_NOT_FOUND);
+		} else if {m.cantidadMaxima<getValorAccion(mc.empresa) {
+			throw new MensajeException (MensajeException.EX_NOT_ENOUGH_DINERO);
+		}else {
+			añadirMensaje(m);
+		}
+	}
+	
+	public void solicitarVenta (string cliente, string empresa, int cantidadAcciones) {
+		mensaje mv = new Mensaje (cliente, empresa, cantidadAcciones)
+		if (bancoCli.nombre.!contains(mv.cliente)) {
+			throw new MensajeException (MensajeException.EX_CLIENTE_NOT_FOUND);
+		} else if {m.cantidadAcciones>getNumTitulos(mv.cliente) {                     //Cuando se termine la clase PaqueteDeAcciones se termina esto (nºtitulos a vender> nºtitulos que posee de esa empresa)
+			throw new MensajeException (MensajeException.EX_NOT_ENOUGH_ACCIONES);
+		}else {
+			añadirMensaje(m);
+		}
+	}
+		
+	public void solicitarActualizacion (string cliente, string empresa, string fecha) {
+		mensaje ma = new Mensaje (cliente, empresa)
+		if (bancoCli.nombre.!contains(mc.cliente)) {
+			throw new MensajeException (MensajeException.EX_CLIENTE_NOT_FOUND);
+		}else {
+			añadirMensaje(m);
+		}
+	// falta la fecha
+	}	
+
 }
