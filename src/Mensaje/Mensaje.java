e335b7d2a66a7ef8bc3f68d6baea0fb4035ab5b6
Daniel Lahera <32450130+DaniL97@users.noreply.github.com>
2018-01-04 22:00:05 +0100

    Update Mensaje.java

--- src/Mensaje/Mensaje.java
+++ src/Mensaje/Mensaje.java
@@ -5,10 +5,46 @@
  */
 package Mensajes;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author dpemar
+ * @author dlahera
  */
 public class Mensaje {
     
+    private int ID;
+	private string cliente;
+	private string empresa;
+
+	public Mensaje (int ID, string cliente, string empresa) {
+		this.ID = ID;
+		this.cliente = cliente;
+		this.empresa = empresa;
+	}
+
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public string getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(string cliente) {
+		this.cliente = cliente;
+	}
+
+	public string getEmpresa() {
+		return empresa;
+	}
+	
+	public void setEmpresa(string Empresa) {
+		this.empresa = empresa;
+	}
+
 }
